dc0ccd3827be4368bdd4f73606f874e45d07ac9f
Sandeep <sd769113@gmail.com>
2020-08-09 09:43:13 +0530

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const forecast = require('./utlis/forecast')
 
 const chalk = require('chalk')
 const app = express()
+
+const port = process.env.PORT || 3000
+
 // define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -92,6 +95,6 @@ app.get('*',(req,res) => {
     })
     
 })
- app.listen(3000, () => {
-     console.log(chalk.blue('server is up on port 3000.'))
+ app.listen(port, () => {
+     console.log(chalk.blue('server is up on port' + port ))
  })
